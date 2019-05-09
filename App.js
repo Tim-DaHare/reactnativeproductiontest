9261a645d3224bd289bfa2f4f18aa0dc475e06dd
Tim de Haas <tim.de.haas@hotmail.nl>
2019-05-09 09:50:52 +0200

    react-router-native

--- App.js
+++ App.js
@@ -5,9 +5,11 @@
 
 import React from 'react'
 import { createAppContainer, createStackNavigator, createBottomTabNavigator } from 'react-navigation'
+import { NativeRouter, Route, Link } from "react-router-native";
 import { View, Text } from 'react-native'
 import { style } from './styles'
-import Navigation from './components/navigation.js'
+
+import Navigation from './components/navigation'
 
 const HomeScreen = () => (
     <View style={style.app__container}>
@@ -33,42 +35,48 @@ const ProfileScreen = () => (
     </View>
 )
     
-const AppNavigator = createBottomTabNavigator(
-    {
-        Home: {
-            screen: HomeScreen,
-            navigationOptions: () => ({
-                title: 'Home'
-            })
-        },
-        Places: {
-            screen: PlacesScreen,
-            navigationOptions: () => ({
-                title: 'Places'
-            })
-        },
-        Live: {
-            screen: LiveScreen,
-            navigationOptions: () => ({
-                title: 'Live'
-            })
-        },
-        Profile: {
-            screen: ProfileScreen,
-            navigationOptions: () => ({
-                title: 'Profile'
-            })
-        },
-    },
-    {
-        initialRouteName: 'Home',
-        // tabBarComponent: props => {
-        //     console.log('props', props)
-        //     return (
-        //         <View style={style.navigation__container}/>
-        //     )
-        // }
-    }
+// const AppNavigator = createBottomTabNavigator(
+//     {
+//         Home: {
+//             screen: HomeScreen,
+//             navigationOptions: () => ({
+//                 title: 'Home'
+//             })
+//         },
+//         Places: {
+//             screen: PlacesScreen,
+//             navigationOptions: () => ({
+//                 title: 'Places'
+//             })
+//         },
+//         Live: {
+//             screen: LiveScreen,
+//             navigationOptions: () => ({
+//                 title: 'Live'
+//             })
+//         },
+//         Profile: {
+//             screen: ProfileScreen,
+//             navigationOptions: () => ({
+//                 title: 'Profile'
+//             })
+//         },
+//     },
+//     {
+//         initialRouteName: 'Home',
+//     }
+// )
+
+// const App = createAppContainer(AppNavigator)
+
+const App = () => (
+    <NativeRouter>
+        <Route exact path="/" component={HomeScreen} />
+        <Route path="/places" component={PlacesScreen} />
+        <Route path="/live" component={LiveScreen} />
+        <Route path="/profile" component={ProfileScreen} />
+        <Navigation />
+    </NativeRouter>
 )
 
-export default createAppContainer(AppNavigator)
+export default App
--- components/navigation.js
+++ components/navigation.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { NativeRouter, Route, Link } from "react-router-native";
 import { style } from '../styles'
 
 const Navigation = () => (
     <View style={style.navigation__container}>
-        <View style={style.navigation__button}><Text>Home</Text></View>
-        <View style={style.navigation__button}><Text>Places</Text></View>
-        <View style={style.navigation__button}><Text>Live</Text></View>
-        <View style={style.navigation__button}><Text>Profile</Text></View>
+        <Link to="/" style={style.navigation__button}><Text>Home</Text></Link>
+        <Link to="/places" style={style.navigation__button}><Text>Places</Text></Link>
+        <Link to="/live" style={style.navigation__button}><Text>Live</Text></Link>
+        <Link to="/profile" style={style.navigation__button}><Text>Profile</Text></Link>
     </View>
 )
 
